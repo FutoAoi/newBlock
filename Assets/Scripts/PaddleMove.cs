355f45adab4aa6d044463ba34ba5b49831d3e55c
futo aoi <faoi0803@icloud.com>
2025-06-23 00:32:11 +0900

    [Add]タイトルシーンを追加

---
+++ Assets/Scripts/PaddleMove.cs
@@ -0,0 +1,91 @@
+using System;
+using UnityEngine;
+public class PaddleMove : MonoBehaviour
+{
+    [SerializeField] GameObject BallPrehab;
+    [SerializeField] GameObject GhostPaddlePrehab;
+    [SerializeField] GameObject ShieldPrehab;
+    public float _PaddleSpeed = 10f;  //パドルスピードの変数
+    private float _GhostSpanTime = 5f;
+    private float _Time;
+    [Range(0, 5)] public int _SpeedLevel = 0;
+    [Range(0, 5)] public int _PaddleScaleLevel = 0;
+    [Range(0, 5)] public int _CriateSpanRevel = 0;
+    [Range(0, 1)] public int _GhostLevel = 0;
+    [HideInInspector] public bool _InGame = false;  //ゲーム中かの判定
+    [HideInInspector] public bool _canCriate = false;
+    [HideInInspector] public bool _canGhost = false;
+    [HideInInspector] public bool _canShield = false;
+    private float x;  //水平方向の処理の値を入れる変数
+    private float y;  //垂直方向の処理の値を入れる変数
+    private float clampedX;  //クランプした後のX
+    private float clampedY;  //クランプした後のY
+    private float moveX;
+    private float moveY;
+    private int _breakBlockCount;
+    private Transform _tf;  //Transformを取得するための変数
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);  //このオブジェクトをシーン移行で破棄しない
+        _breakBlockCount = 0;
+    }
+    void Start()
+    {
+        Application.targetFrameRate = 60;  //フレームレートを60に固定
+        _tf = GetComponent<Transform>();  //Transformを取得
+    }
+    void Update()
+    {
+        Move();  //Move()メソッド
+        CriateBall(_CriateSpanRevel);
+        Ghost();
+    }
+    private void Move()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        moveX = x * _PaddleSpeed * Time.deltaTime;
+        clampedX = Mathf.Clamp(_tf.position.x + moveX, -7.5f, 7.5f);
+        if (_InGame == false)  //ゲーム内じゃなければ
+        {
+            y = Input.GetAxisRaw("Vertical");
+            moveY = y * _PaddleSpeed * Time.deltaTime;
+            clampedY = Mathf.Clamp(_tf.position.y + moveY, -4.5f, 4.5f);
+        }
+        else if (_InGame == true)  //ゲーム内だったら
+        {
+            clampedY = -4.3f;
+        }
+        _tf.position = new Vector3(clampedX, clampedY, 0f);
+    }
+    #region 工事中
+    private void Shield()
+    {
+        if (!_canShield) return;
+        //Instantiate(ShieldPrehab,)
+    }
+    #endregion
+    private void Ghost()
+    {
+        if (!_canGhost) return;
+        if (!_InGame) return;
+        _Time += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && _Time > _GhostSpanTime)
+        {
+            Instantiate(GhostPaddlePrehab, this.transform.position, Quaternion.identity);
+            _Time = 0f;
+        }
+    }
+    private void CriateBall(int criatespanrevel)
+    {
+        if (!_canCriate) return;
+        if (_breakBlockCount > 6 - criatespanrevel)
+        {
+            Instantiate(BallPrehab, this.transform.position, Quaternion.identity);
+            _breakBlockCount = 0;
+        }
+    }
+    public void BreakBlockCount()
+    {
+        _breakBlockCount++;
+    }
+}
