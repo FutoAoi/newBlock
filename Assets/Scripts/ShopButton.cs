661d293a068ed9b0beb2eac2c8e0a46af01cffd4
futo aoi <faoi0803@icloud.com>
2025-06-26 02:44:46 +0900

    [Add]ショップにスキル説明欄を追加

--- Assets/Scripts/ShopButton.cs
+++ Assets/Scripts/ShopButton.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 
 public class ShopButton : MonoBehaviour
 {
+    [Header("設定必須")]
     [SerializeField] int SkillNumber;
+    [SerializeField] TextMeshProUGUI _LevelText;
+    [SerializeField] TextMeshProUGUI _ExplamationText;
+    [SerializeField,Multiline(3)] string _Explanation;
+
+    [Header("スピードボタン、スケールボタン用")]
     [SerializeField] float _PaddleSpeedChanger = 1.0f;
     [SerializeField] Vector3 _PaddleScaleChanger = new Vector3(0.1f, 0, 0);
-    [SerializeField] TextMeshProUGUI _LevelText;
 
+    private string _DefaultText = "コインをためてスキルを強化しよう!!";
     private PaddleMove _PaddleMove;
     private GameObject _Paddle;
     private SpriteRenderer _SpriteRenderer;
@@ -22,6 +28,7 @@ public class ShopButton : MonoBehaviour
         _PaddleMove = _Paddle.GetComponent<PaddleMove>();
         _SpriteRenderer = GetComponent<SpriteRenderer>();
         _Defaultcolor = _SpriteRenderer.color;
+        StatusUpdate();
     }
     private void Update()
     {
@@ -43,6 +50,7 @@ public class ShopButton : MonoBehaviour
                     _PaddleMove._canCriate = true;
                     _PaddleMove._CriateSpanLevel++;
                     _LevelText.text = _PaddleMove._CriateSpanLevel.ToString();
+                    _ExplamationText.text = $"{6 - _PaddleMove._CriateSpanLevel}個ブロックを壊すごとにボールを生成する";
                     break;
                 case 3:
                     _PaddleMove._canShield = true;
@@ -63,6 +71,14 @@ public class ShopButton : MonoBehaviour
         {
             _SpriteRenderer.color = Color.white;
             inButton = true;
+            if (SkillNumber == 2)
+            {
+                _ExplamationText.text = $"{6 - _PaddleMove._CriateSpanLevel}個ブロックを壊すごとにボールを生成する";
+            }
+            else
+            {
+                _ExplamationText.text = _Explanation;
+            }
         }
     }
 
@@ -72,6 +88,28 @@ public class ShopButton : MonoBehaviour
         {
             _SpriteRenderer.color = _Defaultcolor;
             inButton = false;
+            _ExplamationText.text = _DefaultText;
+        }
+    }
+    void StatusUpdate()
+    {
+        switch(SkillNumber)
+        {
+            case 0:
+                _LevelText.text = _PaddleMove._SpeedLevel.ToString();
+                break;
+            case 1:
+                _LevelText.text = _PaddleMove._PaddleScaleLevel.ToString();
+                break;
+            case 2:
+                _LevelText.text = _PaddleMove._CriateSpanLevel.ToString();
+                break;
+            case 3:
+                _LevelText.text = _PaddleMove._ShieldLevel.ToString();
+                break;
+            case 4:
+                _LevelText.text = _PaddleMove._GhostLevel.ToString();
+                break;
         }
     }
 }
